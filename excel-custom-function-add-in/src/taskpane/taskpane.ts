af6cdb536a405c172d814ea4d363c47c06909a46
Ankur Thakur <thakur.ank@northeastern.edu>
2023-04-30 05:58:19 -0400

    Added styler

--- excel-custom-function-add-in/src/taskpane/taskpane.ts
+++ excel-custom-function-add-in/src/taskpane/taskpane.ts
@@ -4,12 +4,18 @@
  */
 /* global console, document, Excel, Office */
 import { ethers } from "ethers";
+import { abi, contractAddress } from "./constants.js"
 
 // The initialize function must be run each time a new page is loaded
 Office.initialize = () => {
-  document.getElementById("sideload-msg").style.display = "none";
-  document.getElementById("app-body").style.display = "flex";
-  // document.getElementById("run").onclick = run;
+  // document.getElementById("sideload-msg").style.display = "none";
+  // document.getElementById("app-body").style.display = "flex";
+  // // document.getElementById("run").onclick = run;
+  document.getElementById("show-address").style.visibility = "hidden";
+  // document.getElementById("parallax-1").style.visibility = "hidden";
+  // document.getElementById("parallax-2").style.visibility = "hidden";
+  document.getElementById("parallax-1").style.display = "none";
+  document.getElementById("parallax-2").style.display = "none";
   document.getElementById("connect-wallet").onclick = connectMetaMask;
 };
 
@@ -24,6 +30,12 @@ async function connectMetaMask() {
       const signer = provider.getSigner();
       const address = await signer.getAddress();
       console.log("Connected account:", address);
+      document.getElementById("main-page-div").style.display = "none";
+      document.getElementById("show-address").style.visibility = "visible";
+      document.getElementById("parallax-1").style.display = "block";
+      document.getElementById("parallax-2").style.display = "block";
+
+      getBalance();
     } catch (error) {
       console.error("Failed to connect to MetaMask:", error);
     }
@@ -31,6 +43,23 @@ async function connectMetaMask() {
     console.error("MetaMask is not installed. Please install MetaMask to continue.");
   }
 }
+
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    try {
+      const balance = await provider.getBalance(contractAddress)
+      document.getElementById("address-balance").innerHTML = ethers.utils.formatEther(balance) + " ETH"
+      console.log(ethers.utils.formatEther("Balance: " + balance))
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    //balanceButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+
 
 async function run() {
   try {
